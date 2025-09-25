d430c7dea4ed888b44c155d9fef028108d6a58a8
Abdulrahman00130 <am9880375@gmail.com>
2025-09-25 23:32:54 +0300

    PL: Added DepartmentsController and index action and view

---
+++ Company.PL/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Company.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class DepartmentsController(IDepartmentService _departmentService) : Controller
+    {
+        public IActionResult Index()
+        {
+            var depts = _departmentService.GetAllDepartments();
+            return View(depts);
+        }
+    }
+}
