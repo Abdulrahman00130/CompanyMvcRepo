81bb026f103427ddb26666a88c291dc5d469e68a
Abdulrahman00130 <am9880375@gmail.com>
2025-10-09 00:46:45 +0300

    Added Logout action, and configured layout's navbar to contain the logout button and modal Added ForgetPassword HttpGet Action and view, still working on SendResetPasswordLink Added CheckYourInbox action and view

---
+++ Company.PL/Utilities/EmailSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Company.PL.Utilities
+{
+    public static class EmailSettings
+    {
+        public static void SendEmail(Email email)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("abdo.elkady9999@gmail.com", "wswdujssqwlelirk");
+            client.Send("abdo.elkady9999@gmail.com", email.To, email.Subject, email.Body);
+        }
+    }
+}
